70ee4775ee31c52ddd5b6f0b02cb1934502b6e94
Celia Hsu <celiahsu@eztravel.com.tw>
2021-01-05 17:44:38 +0800

    connect airtable

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,23 @@
  */
 
 // You can delete this file if you're not using it
+
+// import React from "react"
+// import { createGlobalStyle } from "styled-components"
+
+// const GlobalStyle = createGlobalStyle`
+// h1{
+//     color:red;
+//     font-size:4rem;
+// }
+// `
+// export const wrapRootElement = ({ element }) => {
+//   return (
+//     <>
+//       <GlobalStyle />
+//       {element}
+//     </>
+//   )
+// }
+import { wrapRootElement as wrap } from "./root-wrapper"
+const wrapRootElement = wrap
