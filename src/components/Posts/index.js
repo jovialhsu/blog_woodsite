6d8eb882ac7537996ece5e3c552d233fff4e5ca0
Celia Hsu <celiahsu@eztravel.com.tw>
2021-01-11 13:16:56 +0800

    移除console/socialLink修改

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -2,14 +2,14 @@ import React from "react"
 import Post from "./Post"
 // import Banner from '../Banner'
 const Posts = ({ posts, title }) => {
-  console.log(posts, title)
+  //console.log(posts, title)
   return (
     <section className="posts">
       <h3 className="posts-title">{title}</h3>
       <div className="posts-center">
         <article>
           {posts.map(post => {
-            console.log(post.node)
+            //console.log(post.node)
             return <Post key={post.id} {...post} />
           })}
         </article>
--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -9,21 +9,21 @@ const SocialLinks = ({ styleClass }) => {
   return (
     <ul className={styleClass}>
       <li>
-        <a href="https://twitter.com">
+        <a href="https://www.facebook.com/sinling.hsu/">
           <FaFacebookSquare
             className={`${styles.socialIcon} ${styles.facebookIcon}`}
           ></FaFacebookSquare>
         </a>
       </li>
       <li>
-        <a href="https://twitter.com">
+        <a href="https://github.com/jovialhsu">
           <FaGithubSquare
             className={`${styles.socialIcon} ${styles.githubIcon}`}
           ></FaGithubSquare>
         </a>
       </li>
       <li>
-        <a href="https://twitter.com">
+        <a href="https://twitter.com/jovial528">
           <FaTwitterSquare
             className={`${styles.socialIcon} ${styles.twitterIcon}`}
           ></FaTwitterSquare>
