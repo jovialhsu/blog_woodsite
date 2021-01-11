95738f5b45ef3cb1abffb88857c80c4ed0f5b7ce
Celia Hsu <celiahsu@eztravel.com.tw>
2021-01-11 16:33:25 +0800

    fix : gitTalk指向url

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,8 @@ module.exports = {
   siteMetadata: {
     title: `Blog Woodsite`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://woodsite.netlify.app`,
+    author: `@jovialhsu`,
+    siteUrl: `https://hsu-web.com`,
   },
   plugins: [
     `gatsby-plugin-sharp`,
