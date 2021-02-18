671cf321260dcf38b47c437a653cf2c17c1989c4
Celia Hsu <celiahsu@eztravel.com.tw>
2021-02-18 17:48:28 +0800

    調整版面&新增土耳其旅行內容

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,17 @@
 import React from "react"
 import Links from "../constants/links"
+import { IconContext } from "react-icons"
 import { IoMdClose } from "react-icons/io"
 const Sidebar = ({ open, toggle }) => {
   return (
     <aside className={`sidebar ${open ? "showSidebar" : ""}`}>
       <button className="close-btn" onClick={toggle}>
-        <IoMdClose />
+        <IconContext.Provider value={{ color: "white", size: "50px" }}>
+          <IoMdClose />
+        </IconContext.Provider>
       </button>
       <div className="sidebar-container">
-        <Links styleClass="siderbarLinks" />
+        <Links styleClass="sidebarLinks" />
       </div>
     </aside>
   )
