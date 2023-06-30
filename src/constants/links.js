7f05ccb3c7fddbeea66eec91ed03e2052f20fe88
Celia Hsu <celiahsu@eztravel.com.tw>
2023-06-30 16:06:04 +0800

    upd: 更新展覽電影資訊

--- src/constants/links.js
+++ src/constants/links.js
@@ -5,57 +5,61 @@ import styled from "styled-components"
 const Links = ({ styleClass, children }) => {
   return (
     <Wrapper>
-    <ul className={styleClass}>
-      <li>
-        <Link to="/" className="pageLink">
-          Home
-        </Link>
-      </li>
-       {/*<li>
+      <ul className={styleClass}>
+        <li>
+          <Link to="/" className="pageLink">
+            Home
+          </Link>
+        </li>
+        {/*<li>
         <Link to="/post" className="pageLink">
           文章
         </Link>
         {children}
        </li>*/}
-      <li>
-        <Link to="/contact" className="pageLink">
-          Contact
-        </Link>
-      </li>
-      <li>
-        <Link to="/movie" className="pageLink">
-          展覽電影一覽
-        </Link>
-      </li>
-    </ul></Wrapper>
+        <li>
+          <Link to="/contact" className="pageLink">
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link to="/movie" className="pageLink">
+            展覽電影一覽
+          </Link>
+        </li>
+      </ul>
+    </Wrapper>
   )
 }
 const Wrapper = styled.div`
-.sidebarLinks .pageLink {
-  font-size: 2rem;
-  display: block;
-  color: rgb(6, 82, 6);
-  font-weight: 700;
-}
-@media screen and (min-width: 800px) {
-  .pageLink {
-    margin-right: 0.75rem;
+  .sidebarLinks .pageLink {
+    font-size: 2rem;
+    display: block;
+    color: #aabbcc;
+    font-weight: 700;
+    &:hover {
+      color: #d3d8de;
+    }
   }
-  .pageLink {
-    color: #0a0c10;
-    font-weight: bold;
-    letter-spacing: 0.1rem;
-    font-size: 1rem;
-    transition: all 0.2s linear;
-    cursor: pointer;
-    white-space: nowrap;
-    padding: 0.25rem 0.5rem;
-    border-radius: 0.25rem;
+  @media screen and (min-width: 800px) {
+    .pageLink {
+      margin-right: 0.75rem;
+    }
+    .pageLink {
+      color: #0a0c10;
+      font-weight: bold;
+      letter-spacing: 0.1rem;
+      font-size: 1rem;
+      transition: all 0.2s linear;
+      cursor: pointer;
+      white-space: nowrap;
+      padding: 0.25rem 0.5rem;
+      border-radius: 0.25rem;
+    }
+    .pageLink:hover {
+      background: hsl(217, 37%, 57%);
+      color: hsl(0, 0%, 99%);
+    }
   }
-  .pageLink:hover {
-    background: hsl(217, 37%, 57%);
-    color: hsl(0, 0%, 99%);
-  }
-}
 `
 export default Links
--- src/templates/movieCountyTag.js
+++ src/templates/movieCountyTag.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-
+import styled from "styled-components"
 export default function movieCountyTagPage({ pageContext, data }) {
   const { edges } = data.allMovieNode
   const { tag } = pageContext
@@ -10,64 +10,92 @@ export default function movieCountyTagPage({ pageContext, data }) {
   return (
     <Layout>
       <Seo title={header} />
-      <section className="movie-page">
-        <h1>
-          <span role="img" aria-label="sparkle">
-            ✨
-          </span>
-          {tag}展覽電影一覽
-        </h1>
-        <ul>
-          {edges.map(({ node }) => {
-            // console.log(node)
-            return (
-              <li key={node.id} title={node.description}>
-                <div>{node.showInfo[0].time}</div>
-                {node.showInfo[0].onSales === "Y" ? (
-                  node.webSales !== "" ? (
-                    <a href={node.webSales} target="_blank" rel="noreferrer">
+      <Wrapper>
+        <section className="movie-page">
+          <h1>
+            <span role="img" aria-label="sparkle">
+              ✨
+            </span>
+            {tag}展覽電影一覽
+          </h1>
+          <ul>
+            {edges.map(({ node }) => {
+              return (
+                <li key={node.id}>
+                  <div>{node.showInfo[0].time}</div>
+                  {node.showInfo[0].onSales === "Y" ? (
+                    node.webSales !== "" ? (
+                      <a href={node.webSales} target="_blank" rel="noreferrer">
+                        <span
+                          role="img"
+                          aria-label="sale"
+                          title={node.showInfo[0].price}
+                        >
+                          {" "}
+                          💲
+                        </span>
+                      </a>
+                    ) : (
                       <span
                         role="img"
                         aria-label="sale"
                         title={node.showInfo[0].price}
                       >
-                        {" "}
                         💲
                       </span>
-                    </a>
+                    )
                   ) : (
-                    <span
-                      role="img"
-                      aria-label="sale"
-                      title={node.showInfo[0].price}
-                    >
-                      {" "}
-                      💲
-                    </span>
-                  )
-                ) : (
-                  ""
-                )}
-                <span title={node.showInfo[0].location}>
-                  【{node.showInfo[0].locationName}】
-                </span>
-                {node.title}
-              </li>
-            )
-          })}
-        </ul>
+                    ""
+                  )}
+                  <span title={node.showInfo[0].location}>
+                    【{node.showInfo[0].locationName}】
+                  </span>
+                  {node.title}
+                  {node.showInfo[0].location !== "" ? (
+                    <div className="desc">
+                      <span role="img" aria-label="location">
+                        📍
+                      </span>{" "}
+                      地址: {node.showInfo[0].location}
+                    </div>
+                  ) : null}
+                  {node.description !== "" ? (
+                    <div className="desc">
+                      <span role="img" aria-label="moive description">
+                        🎬
+                      </span>
+                      詳情:
+                      <ul>
+                        {node.description.split("。").map((item, i) => {
+                          return (
+                            <li>
+                              {item}
+                              {i !== node.description.split("。").length - 1
+                                ? "。"
+                                : null}
+                            </li>
+                          )
+                        })}
+                      </ul>
+                    </div>
+                  ) : null}
+                </li>
+              )
+            })}
+          </ul>
 
-        {/*
+          {/*
             This links to a page that does not yet exist.
             You'll come back to it!
           */}
-        <Link to="/movie">
-          <span role="img" aria-label="movie">
-            🎬
-          </span>
-          看全部縣市
-        </Link>
-      </section>
+          <Link to="/movie">
+            <span role="img" aria-label="movie">
+              🎬
+            </span>
+            看全部縣市
+          </Link>
+        </section>
+      </Wrapper>
     </Layout>
   )
 }
@@ -95,6 +123,16 @@ export const pageQuery = graphql`
           title
           webSales
         }
+      }
+    }
+  }
+`
+const Wrapper = styled.section`
+  .desc {
+    font-size: 13px;
+    ul {
+      li {
+        margin-bottom: calc(1.45rem / 3);
       }
     }
   }
